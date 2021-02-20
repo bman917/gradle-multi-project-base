2ee4cecbbfa85851172844d713bdf17488773b13
jchan <Jacky.Chan@Datalex.com>
2021-02-20 00:33:45 +0000

    initial

---
+++ 01-child-apps/hello/src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run() {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				System.out.println("Hello!");
+			}
+		};
+	}
+
+}

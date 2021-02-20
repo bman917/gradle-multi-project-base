2ae62a059593cb775ef97e64a325e7728e61afc1
jchan <Jacky.Chan@Datalex.com>
2021-02-20 00:37:21 +0000

    Add sample dependency

---
+++ 01-child-apps/hello/src/main/java/com/example/demo/DemoImpl.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import com.Demo;
+
+public class DemoImpl implements Demo {
+    @Override
+    public void foo() {
+        System.out.println("DemoImpl FOO....");
+
+    }
+}
